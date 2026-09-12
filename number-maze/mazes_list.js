fc8d7d5545f2ec9393c243b1008b858ebd4b6ecf
Derek Liu <derekl@mit.edu>
2026-09-11 23:52:00 -0400

    actually explained scratch work

--- number-maze/mazes_list.js
+++ number-maze/mazes_list.js
@@ -64,7 +64,13 @@ const levels = [
     },
     {
         title: "Tutorial #3",
-        intro_text: `When it comes to some operations, <b>order matters</b>!`,
+        intro_text: `When it comes to some operations, <b>order matters</b>!
+        
+        (Some additional controls: you can <b>drag</b> the red square around to move.
+        Dragging anywhere else on the grid creates <b>scratch work</b> in the form of <b><span style="color:lightgreen">green</span></b> lines.
+        If you have a green path <b>leading to the ending square</b>, a <b><span style="color:green">green number</span></b> will show up.
+        This number shows you the number you'd need to follow that green path to win!
+        Scratch work is <b>purely cosmetic</b>; you'll still have to get the red square to the finish.)`,
         maze: [
             ["", "", "x3", ""],
             ["", "", "", ""],
